9e9ba5ca69abb321315c085a014cd9cdb3fe6962
burton <burt.alexander@gmail.com>
2014-04-27 13:56:57 +1000

    speling fics

--- ESP-Emotiv/src/com/github/mrstampy/esp/emotiv/EmotivConstants.java
+++ ESP-Emotiv/src/com/github/mrstampy/esp/emotiv/EmotivConstants.java
@@ -1,5 +1,5 @@
 /*
- * ESP-Emotive Copyright (C) 2014 Burton Alexander
+ * ESP-Emotiv Copyright (C) 2014 Burton Alexander
  * 
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
